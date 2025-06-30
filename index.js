77c79212e0dfb6aaefd0c06ac6037a5db329b4ba
Md-shahien-khan <shawonkn58@gmail.com>
2025-07-01 00:02:43 +0100

    added functionality for job applications and own application

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
 
     // Job Related API
     const jobsCollection = client.db('jobseek').collection('jobs');
+    const jobsApplication = client.db('jobseek').collection('job_application');
 
     // get jobs
     app.get('/jobs', async(req, res)=>{
@@ -48,7 +49,40 @@ async function run() {
         const query = {_id: new ObjectId(id)}
         const result = await jobsCollection.findOne(query);
         res.send(result);
+    });
+
+    // job application
+    app.post('/job-applications', async(req, res) =>{
+      const application = req.body;
+      const { job_id, applicant_email } = application;
+      // if someone already applied the job
+      const existing = await jobsApplication.findOne({job_id, applicant_email});
+      if(existing){
+        return res.send({success : false, message : "Already Applied"});
+      }
+      const result = await jobsApplication.insertOne(application);
+      res.send(result);
+    });
+
+
+    // your own applied jobs
+    app.get('/job-application', async(req, res) =>{
+      const email =  req.query.email;
+      const query = { applicant_email : email};
+      const result = await jobsApplication.find(query).toArray();
+      res.send(result);
     })
+
+
+    // check if the user already applied for that job
+    // app.get('/job-applicatio/check', async(req, res) =>{
+    //   const {job_id, email} = req.body;
+    //   if(!job_id || !email){
+    //     return res.status(400).send({applied : false});
+    //   }
+    //   const existing = await jobsApplication.findOne({job_id, applicant_email : email});
+    //   res.send({applied : !existing});
+    // })
 
 
 
